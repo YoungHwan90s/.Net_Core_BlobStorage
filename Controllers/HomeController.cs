32f8ea1eb13369eaf03b3f7de319fc42bab4ce27
YoungHwan90s <younghwan1608@gmail.com>
2024-03-15 11:06:23 +0900

    [UPDATE] Image file resizing

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using _NetCoreBlob.Models;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats.Jpeg;
+
 
 namespace _NetCoreBlob.Controllers;
 
@@ -36,16 +39,6 @@ public class HomeController : Controller
         // 저장 될 컨테이너 저장소 이름
         string containerName  = "web";
 
-        var containerClient = _blobService.GetBlobContainerClient(containerName);
-
-        // 컨테이너가 존재하지 않는 경우에 컨테이너 생성 시도
-        if (!await containerClient.ExistsAsync())
-        {
-            // 컨테이너 생성
-            // PublicAccessType.Blob - Blob에 대한 읽기 전용 액세스 허용
-            await containerClient.CreateIfNotExistsAsync(PublicAccessType.Blob);
-        }
-
         // 고유 이름 생성
         //var blobName = $"{Guid.NewGuid().ToString()}.jpg";
 
@@ -54,16 +47,52 @@ public class HomeController : Controller
 
         try
         {
-            // 이미지 Blob에 업로드하기 전에 같은 이름의 파일이 있는지 확인하고 삭제
+            var containerClient = _blobService.GetBlobContainerClient(containerName);
+
+            // 1. 컨테이너 없을 경우 생성 - PublicAccessType.Blob(Blob에 대한 읽기 전용 액세스 허용)
+            await containerClient.CreateIfNotExistsAsync(PublicAccessType.Blob);
+
+            // 2. 이미지 Blob에 업로드하기 전에 같은 이름의 파일이 있는지 확인하고 삭제
             var blobClient = containerClient.GetBlobClient(blobName);
             if (await blobClient.ExistsAsync())
             {
                 await blobClient.DeleteIfExistsAsync();
             }
-            
-            // 이미지 Blob에 업로드
-            // 아래 작업이 실패하면 error를 던지기 때문에 catch문으로 이동 됨
-            await containerClient.UploadBlobAsync(blobName, file.OpenReadStream());
+
+            // 3.이미지 처리 및 리사이징
+            using (var image = SixLabors.ImageSharp.Image.Load(file.OpenReadStream()))
+            {
+                // 4. 이미지 파일의 크기를 확인
+                var fileSize = file.Length;
+
+                // 5. 2MB 보다 크면 리사이징 작업
+                if (fileSize > (1024 * 1024 * 2))
+                {
+                    // // 5.1 이미지 리사이징
+                    //image.Mutate(x => x.Resize(500, 500)); // 너비 100px, 높이 100px로 리사이징
+
+                    // 5.2 압축 품질 설정
+                    int compressionQuality = 80;
+
+                    // 5.3 메모리 스트림에 이미지 저장
+                    using (var compressedStream = new MemoryStream())
+                    {
+                        image.Save(compressedStream, new SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder()
+                        {
+                            Quality = compressionQuality // 압축 품질 설정 (0 ~ 100)
+                        });
+
+                        compressedStream.Position = 0;
+
+                        // 6. 이미지 Blob에 업로드
+                        await containerClient.UploadBlobAsync(blobName, compressedStream);
+                    }
+                }
+                else
+                {
+                    await containerClient.UploadBlobAsync(blobName, file.OpenReadStream());
+                }
+            }
 
             // 반환 이미지 URL
             blobUrl = blobClient.Uri.ToString();
@@ -72,16 +101,6 @@ public class HomeController : Controller
         {
             throw new Exception(ex.Message);
         }
-
-        // // 업로드 성공 여부에 따라 적절한 응답 반환
-        // if (uploadSuccessful)
-        // {
-        //     return Ok("이미지 업로드 성공");
-        // }
-        // else
-        // {
-        //     return BadRequest("이미지 업로드 실패");
-        // }
 
         return blobUrl;
     }
